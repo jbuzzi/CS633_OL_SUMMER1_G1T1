8d40a54d6b0e6dd15b0c842d71d48354f964a443
Elizabeth Lynn Rakphongphairoj Kilrain <elizabeth@beehive.codes>
2016-06-16 00:17:43 -0700

    Styles and scripts
    CSS and Scripts for views and controllers

---
+++ app/js/form.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('input, select').blur(function (e) {
+        $(this).val($(this).val().trim());
+        var valid = e.target.checkValidity();
+        console.log('valid: ', valid);
+        if (valid) {
+            var parentGroup = $(this).closest('.form-group');
+            parentGroup.removeClass('has-error');
+            parentGroup.removeClass(function (index, classes) {
+                return classes.split(/\s+/).filter(function (el) {
+                    return /^error-/.test(el);
+                }).join(' ');
+            });
+        }
+    }).bind('invalid', function (e) {
+        e.target.setCustomValidity('');
+        var parentGroup = $(this).closest('.form-group');
+        parentGroup.addClass('has-error');
+        if (e.target.validity.valueMissing) {
+            e.target.setCustomValidity($(this).data('error-required'));
+            parentGroup.addClass('error-required');
+        } else if (e.target.validity.patternMismatch) {
+            e.target.setCustomValidity($(this).data('error-pattern'));
+            parentGroup.addClass('error-pattern');
+        } else if (e.target.validity.typeMismatch) {
+            e.target.setCustomValidity($(this).data('error-type'));
+            parentGroup.addClass('error-type');
+        }
+    });
+});
